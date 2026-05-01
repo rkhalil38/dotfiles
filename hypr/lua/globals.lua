13cd85d76065ff6acec7d0d8d53534b6b1b1712b
rkhalil38 <romuluskhalil3@gmail.com>
2026-05-01 15:40:27 -0400

    globals: fixed command menu

--- hypr/lua/globals.lua
+++ hypr/lua/globals.lua
@@ -7,7 +7,7 @@ M.terminal = "ghostty"
 M.browser = "firefox"
 M.file_manager = "thunar"
 M.menu = "rofi -show drun -show-icons -icon-theme \"Kora\""
-M.command_menu = "rofi -show-run"
+M.command_menu = "rofi -show run"
 M.screenshot_clipboard = "hyprshot -m region --clipboard-only"
 M.screenshot_downloads = "hyprshot -m region -o ~/Downloads/"
 
