508c132999ed8d13c1e47879ab5fd92178f0f0de
rkhalil38 <romuluskhalil3@gmail.com>
2026-05-17 23:08:26 -0400

    monitors: samsung tv

--- hypr/lua/monitors.lua
+++ hypr/lua/monitors.lua
@@ -4,6 +4,14 @@
 hl.monitor({
     output = "desc:Microstep MSI MAG 275QF CE2M144701107",
     mode = "2560x1440@180",
-    position = "auto",
+    position = "2560x0",
     scale = 1
+})
+
+hl.monitor({
+    output = "desc:Samsung Electric Company SAMSUNG",
+    mode = "3840x2160@60",
+    position = "0x0",
+    scale = 1.5,
+    bitdepth = 10
 })
