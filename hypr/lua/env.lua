2f76296ad20634b6274ae02843c291c7297d0ea3
rkhalil38 <romuluskhalil3@gmail.com>
2026-05-21 18:37:09 -0400

    util: environment lib

--- hypr/lua/env.lua
+++ hypr/lua/env.lua
@@ -1,13 +1,20 @@
+local globals = require("lua.globals")
+local env = require("lua.util.environment")
+
 --------------------
 ----- ENV VARS -----
 --------------------
-local globals = require("lua.globals")
 
-hl.env("XCURSOR_SIZE", globals.cursor.size)
-hl.env("XCURSOR_THEME", globals.cursor.theme)
-hl.env("HYPRCURSOR_SIZE", globals.cursor.size)
-hl.env("HYPRCURSOR_THEME", globals.cursor.theme)
-hl.env("LIBVA_DRIVER_NAME", "nvidia")
-hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
-hl.env("QT_QPA_PLATFORMTHEME", "hyprqt6engine")
-hl.env("TERM", globals.terminal)
+--- @type EnvVar[]
+local vars = {
+    { name = "XCURSOR_SIZE", val = globals.cursor.size },
+    { name = "XCURSOR_THEME", val = globals.cursor.theme },
+    { name = "HYPRCURSOR_SIZE", val = globals.cursor.size },
+    { name = "HYPRCURSOR_THEME", val = globals.cursor.theme },
+    { name = "LIBVA_DRIVER_NAME", val = "nvidia" },
+    { name = "__GLX_VENDOR_LIBRARY_NAME", val = "nvidia" },
+    { name = "QT_QPA_PLATFORMTHEME", val = "hyprqt6engine" },
+    { name = "TERM", val = globals.terminal },
+}
+
+env.set_vars(vars)
---
+++ hypr/lua/util/environment.lua
@@ -0,0 +1,32 @@
+--- Class representing an Env variable.
+--- @class EnvVar
+--- @field name string
+--- @field val string|number
+
+--- Utility for configuring Hyprland environment variables.
+--- @class EnvUtil
+local M = {}
+
+--- Normalizes env vars.
+--- @param vars EnvVar|EnvVar[]
+--- @return EnvVar[]
+local function normalize_envs(vars)
+    if vars[1] ~= nil then
+        return vars
+    end
+
+    return { vars }
+end
+
+--- Sets an environment variable.
+--- @param vars EnvVar|EnvVar[]
+M.set_vars = function(vars)
+    vars = normalize_envs(vars)
+
+    for _, var in ipairs(vars) do
+        hl.env(var.name, var.val)
+    end
+end
+
+--- @type EnvUtil
+return M
